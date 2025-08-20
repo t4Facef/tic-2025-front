332f9f8339ed66f92817f2a6b60ae88211cc5651
LucianoMazar <lmazaraojr@gmail.com>
2025-08-19 21:08:45 -0300

    Finalizando a pagina de lista de vagas

--- src/components/content/search_box.tsx
+++ src/components/content/search_box.tsx
@@ -2,9 +2,9 @@ import {Search} from "lucide-react"
 
 export default function SearchBox(){
     return(
-        <form className="flex max-h-[2.9rem]">
-            <input type="text" name="search" id="search" placeholder="Pesquisar" className="border border-black rounded-l-lg w-[55rem] pl-5"/>
-            <button className="flex bg-blue4 border border-black rounded-r-lg p-2 items-center">{<Search/>}</button>
+        <form className="flex h-[3.5rem]">
+            <input type="text" name="search" id="search" placeholder="Pesquisar" className="border border-black rounded-l-lg w-[112vh] pl-5"/>
+            <button className="flex bg-blue4 border border-black rounded-r-lg p-2 items-center justify-center w-[8vh]">{<Search/>}</button>
         </form>
     )
 }
--- src/pages/positions_list.tsx
+++ src/pages/positions_list.tsx
@@ -1,13 +1,25 @@
+// [TODO] - Configurar a Search box para não ultrapassar o limite da pagina quando com muito zoom
+
+import JobPosition from "../components/content/job_position";
 import SearchBox from "../components/content/search_box";
 
 export default function PositionsList(){
     return (
         <div className="flex flex-1">
-            <div className="bg-blue2 flex-[1] flex justify-center text-[16px] p-1 min-h-screen">
+            <div className="bg-blue2 flex-[2] flex justify-center text-[16px] p-1 min-h-screen">
                 <p className="text-white">Filtros</p>
             </div>
-            <div className="flex flex-[7] justify-center mt-7 mx-5">
+            <div className="flex flex-[11] flex-col items-center mt-7 mx-5">
                 <SearchBox/>
+                <div>
+                    <JobPosition profile="" title="Lorem Impsum">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quidem cum eum facere laudantium pariatur enim officia esse. Doloribus sed minus sunt nesciunt incidunt cumque perferendis maiores. Fuga, assumenda veniam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi doloremque corrupti ratione ut doloribus et hic, adipisci nostrum architecto est nesciunt exercitationem! Aspernatur error odit vero esse ad molestiae amet.</JobPosition>
+                    <JobPosition profile="" title="Lorem Impsum">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quidem cum eum facere laudantium pariatur enim officia esse. Doloribus sed minus sunt nesciunt incidunt cumque perferendis maiores. Fuga, assumenda veniam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi doloremque corrupti ratione ut doloribus et hic, adipisci nostrum architecto est nesciunt exercitationem! Aspernatur error odit vero esse ad molestiae amet.</JobPosition>
+                    <JobPosition profile="" title="Lorem Impsum">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quidem cum eum facere laudantium pariatur enim officia esse. Doloribus sed minus sunt nesciunt incidunt cumque perferendis maiores. Fuga, assumenda veniam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi doloremque corrupti ratione ut doloribus et hic, adipisci nostrum architecto est nesciunt exercitationem! Aspernatur error odit vero esse ad molestiae amet.</JobPosition>
+                    <JobPosition profile="" title="Lorem Impsum">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quidem cum eum facere laudantium pariatur enim officia esse. Doloribus sed minus sunt nesciunt incidunt cumque perferendis maiores. Fuga, assumenda veniam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi doloremque corrupti ratione ut doloribus et hic, adipisci nostrum architecto est nesciunt exercitationem! Aspernatur error odit vero esse ad molestiae amet.</JobPosition>
+                </div>
+                <div>
+                    <span></span>
+                </div>
             </div>
         </div>
     )
