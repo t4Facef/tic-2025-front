67821df3d1bffaa8a1e6ef0602f83514716e6479
LucianoMazar <lmazaraojr@gmail.com>
2025-08-22 20:58:31 -0300

    Aplicação de rounded nas vagas

--- src/components/content/job_position.tsx
+++ src/components/content/job_position.tsx
@@ -13,16 +13,16 @@ export default function JobPosition({ profile, title, children }: JobPositionPro
   return (
     <>
       <div className={`max-w-[140vh] min-w-[140vh] flex pt-8 cursor-pointer transition-opacity ${!open ? 'hover:opacity-75' : ''}`} onClick={() => setOpen(true)} >
-        <div className="w-28 h-28 bg-orange1 border-black border-2"> 
+        <div className="w-28 h-28 bg-orange1 border-black border-2 rounded-l-md"> 
           <img src={profile} alt="perfil-img" />
         </div>
         
         <div className="flex flex-col flex-1 max-h-28">
-          <div className="bg-orange1 border-black border-2 flex-[1] flex justify-between items-center px-2">
+          <div className="bg-orange1 border-black border-2 flex-[1] flex justify-between items-center px-2 rounded-tr-md">
             <span className="font-georgia">{title}</span>
           </div>
 
-          <div className="bg-orange2 border-black border-2 flex-[3] overflow-auto p-2 break-words">
+          <div className="bg-orange2 border-black border-2 flex-[3] overflow-auto p-2 break-words rounded-br-md">
             <p className="font-georgia">{children}</p>
           </div>
         </div>
