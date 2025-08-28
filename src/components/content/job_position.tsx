d63c4479687936df31e48928dc4f5a37d688b29a
LucianoMazar <lmazaraojr@gmail.com>
2025-08-27 21:26:32 -0300

    Teste mudança de cor das vagas

--- src/components/content/job_position.tsx
+++ src/components/content/job_position.tsx
@@ -13,16 +13,16 @@ export default function JobPosition({ profile, title, children }: JobPositionPro
   return (
     <>
       <div className={`w-full flex pt-8 cursor-pointer transition-all duration-300 ease-in-out transform hover:scale-[1.02] ${!open ? 'hover:opacity-75' : ''}`} onClick={() => setOpen(true)} >
-        <div className="w-28 h-28 bg-orange1 border-black border-2 rounded-l-md"> 
+        <div className="w-28 h-28 bg-blue2 border-blue3 border-2 rounded-l-md"> 
           <img src={profile} alt="perfil-img" />
         </div>
         
         <div className="flex flex-col flex-1 max-h-28">
-          <div className="bg-orange1 border-black border-2 flex-[1] flex justify-between items-center px-2 rounded-tr-md">
+          <div className="bg-blue2 border-blue3 border-2 flex-[1] flex justify-between items-center px-2 rounded-tr-md">
             <span className="font-georgia">{title}</span>
           </div>
 
-          <div className="bg-orange2 border-black border-2 flex-[3] overflow-auto p-2 break-words rounded-br-md">
+          <div className="bg-blue1 border-blue3 border-2 flex-[3] overflow-auto p-2 break-words rounded-br-md">
             <p className="font-georgia">{children}</p>
           </div>
         </div>
