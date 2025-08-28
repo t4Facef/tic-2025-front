035b5b999a37e7de4fa668ebcf234fc09d5b636b
jpedrolourenco08 <jpedro.lourenco08@gmail.com>
2025-08-28 20:56:09 -0300

    Refatoração da página job_new e mínima alteração no tag_container

--- src/components/content/tag_container.tsx
+++ src/components/content/tag_container.tsx
@@ -8,8 +8,8 @@ interface TagContainerProps {
 
 export default function TagContainer({children, tags = []}: TagContainerProps){
     return(
-        <div className="mt-5">
-            <p className="inline-flex bg-blue2 text-black p-2 px-3 rounded-t-3xl">{children}</p>
+        <div className="mt-5 w-full">
+            <p className="inline-flex bg-blue2 text-black p-2 px-3 rounded-t-xl">{children}</p>
             <div className="border-[2px] border-black min-h-[10rem] rounded-b-xl p-3 gap-2 bg-white">
                 {tags.map((tag, index) => (
                     <Tag key={index}>{tag}</Tag>
--- src/pages/job_new.tsx
+++ src/pages/job_new.tsx
@@ -1,11 +1,60 @@
-import JobRegistrationForm from "../components/forms/register/job_registation_form";
-
-export default function JobNew() {
+import GenericFormField from "../components/forms/generic_form_field";
+import GenericBlueButton from "../components/buttons/generic_blue_button";
+import TagContainer from "../components/content/tag_container";
+  
+export default function JobRegistrationForm() {
   return (
-    <div className="p-10">
-      <div className="bg-blue1 rounded-lg mx-24">
-        <JobRegistrationForm/>
+    <div className="my-16 mx-36">
+      <div className="p-3 text-white text-[1.5rem] bg-blue3 border border-black min-w-[28rem] rounded-t-lg">
+        <h2>
+        Cadastre sua vaga
+      </h2>
       </div>
+      <form className="flex-col text-start space-5 text-blue3 bg-blue1 rounded-b-lg px-4">
+      <div className="p-3 space-y-5">
+        <GenericFormField id="job_registration" type="text" placeholder="Titulo da vaga">
+          Nome da vaga
+        </GenericFormField>
+        <div className="flex gap-24">
+          <GenericFormField id="start_date" type="date">
+            Início das inscrições
+          </GenericFormField>
+          <GenericFormField id="end_date" type="date">
+            Fim das inscrições
+          </GenericFormField>
+        </div>
+        <GenericFormField id="job_description" type="textarea" placeholder="Descreva sua vaga aqui...">
+          Descrição da vaga
+        </GenericFormField>  
+        <GenericFormField id="job_salary" type="text" placeholder="R$0,00">
+          Salário
+        </GenericFormField>
+        <GenericFormField
+          id="job_level"
+          type="select"
+          options={["Estágio", "Júnior", "Pleno", "Sênior"]}
+        >
+          Nível da vaga
+        </GenericFormField>
+        <GenericFormField id="job_workload" type="text" placeholder="Horas">
+          Carga Horária
+        </GenericFormField>
+        <GenericFormField
+          id="job_characteristics"
+          type="radio"
+          options={["Remoto", "Presencial", "Híbrido"]}
+        >
+          Características da vaga
+        </GenericFormField>
+        <TagContainer tags={["Java", "Python", "C++", "C#", "JavaScript", "PHP", "Ruby", "Go", "Rust", "Swift", "Kotlin", "TypeScript", "Scala", "Perl", "R", "Haskell", "Lua", "Elixir", "Clojure", "Dart", "Julia"]}>
+          Habilidades
+        </TagContainer>
+      </div>
+      <div className="flex justify-end p-4">
+        <GenericBlueButton color={3}>Cadastrar Vaga</GenericBlueButton>
+      </div>
+    </form>
     </div>
+    
   );
 }
