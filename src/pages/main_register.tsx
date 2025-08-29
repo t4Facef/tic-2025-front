9ed6bd1e98856a82042705ee05ee15a85cbda20c
LucianoMazar <lmazaraojr@gmail.com>
2025-08-29 19:19:20 -0300

    Adição de caminho alternativo na pagina inicial de registro

--- src/pages/main_register.tsx
+++ src/pages/main_register.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function MainRegister() {
   const navigate = useNavigate()
@@ -14,15 +14,21 @@ export default function MainRegister() {
           gente quem você é!
         </p>
       </div>
-      <div  className="flex flex-row justify-center py-8 space-x-8 my-8">
-        <button className="bg-blue3 rounded-l-xl p-16 space-x-5 hover:bg-blue3H transition-colors" onClick={() => navigate("/auth/register/candidates")}>
-          <p className="text-blue1 font-semibold text-[1.5rem]">Responsável da Empresa</p>
-          <p className="text-white">buscando por talentos especiais</p>
-        </button>
-        <button className="bg-blue5 rounded-r-xl p-16 hover:bg-blue5H transition-colors" onClick={() => navigate("/auth/register/companies")}>
-          <p className=" text-blue3 font-semibold text-[1.5rem] px-8">Candidato PCD</p>
-          <p>buscando oportunidades inclusivas</p>
-        </button>
+      <div className="flex flex-col justify-center items-center py-8 space-x-8 my-8 space-y-12">
+        <div className="flex flex-row ">
+          <button className="bg-blue3 rounded-l-xl p-16 space-x-5 hover:bg-blue3H transition-colors" onClick={() => navigate("/auth/register/candidates")}>
+            <p className="text-blue1 font-semibold text-[1.5rem]">Responsável da Empresa</p>
+            <p className="text-white">buscando por talentos especiais</p>
+          </button>
+          <button className="bg-blue5 rounded-r-xl p-16 hover:bg-blue5H transition-colors" onClick={() => navigate("/auth/register/companies")}>
+            <p className=" text-blue3 font-semibold text-[1.5rem] px-8">Candidato PCD</p>
+            <p>buscando oportunidades inclusivas</p>
+          </button>
+        </div>
+        <div className="flex flex-col items-center">
+          <p>Não é sua primeira vez?</p>
+          <Link to={"/"} className="underline font-bold">Clique aqui</Link>
+        </div>
       </div>
     </div>
   );
