c91242095a8b25ac3c1c799010aef22f248a3f56
LucianoMazar <lmazaraojr@gmail.com>
2025-09-02 20:25:49 -0300

    Correção pagina login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,8 +6,8 @@ export default function Login(){
     return (
         <div className="flex justify-center">
             <div className="bg-blue1 border border-black min-w-[28rem] my-12 rounded-md">
-                <span className="flex justify-center border-b border-black text-[20px] font-semibold py-2">Login</span>
-                <div className="flex justify-center pb-5">
+                <span className="flex justify-center border-b border-black text-white text-[20px] font-semibold py-2 bg-blue3">Login</span>
+                <div className="flex justify-center py-5">
                     <form className="flex flex-col min-w-[18rem] space-y-4">
                         <GenericFormField id="user" type="text" placeholder="E-mail, CPF ou CNPJ" autoComplete="username">Usuário</GenericFormField>
                         <GenericFormField id="senha" type="password" placeholder="Digite sua senha" autoComplete="current-password">Senha</GenericFormField>
@@ -18,7 +18,7 @@ export default function Login(){
                                 <span> Lembre de mim</span>
                             </label>
                         </div>
-                        <div className="flex justify-between items-end pt-32">
+                        <div className="flex justify-between items-end pt-20">
                             <div className="flex flex-col text-[13px]">
                                 <span>É sua primeira vez?</span>
                                 <Link to="/auth/register/main" className="underline pt-1">CADASTRAR</Link>
