104935b869d2694792e32f1c9682edbd129c6905
LucianoMazar <lmazaraojr@gmail.com>
2025-09-05 19:38:22 -0300

    Correção da pagina de login para maior responsividade

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,29 +4,34 @@ import GenericFormField from "../components/forms/generic_form_field";
 
 export default function Login(){
     return (
-        <div className="flex justify-center text-blue3">
-            <div className="bg-blue1 border border-black min-w-[28rem] my-12 rounded-md">
-                <span className="flex justify-center border-b border-black text-white text-[20px] font-semibold py-2 bg-blue3">Login</span>
-                <div className="flex justify-center py-5">
-                    <form className="flex flex-col min-w-[18rem] space-y-4">
-                        <GenericFormField id="user" type="text" placeholder="E-mail, CPF ou CNPJ" autoComplete="username">Usuário</GenericFormField>
-                        <GenericFormField id="senha" type="password" placeholder="Digite sua senha" autoComplete="current-password">Senha</GenericFormField>
-                        <div className="flex justify-between text-[14px] py-2">
-                            <Link to="/auth/password/forgot" className="underline">Esqueci a senha</Link>
-                            <label htmlFor="rememberMe">
-                                <input type="checkbox" id="rememberMe"/>
-                                <span> Lembre de mim</span>
-                            </label>
-                        </div>
-                        <div className="flex justify-between items-end pt-20">
-                            <div className="flex flex-col text-[13px]">
-                                <span>É sua primeira vez?</span>
-                                <Link to="/auth/register/main" className="underline pt-1">CADASTRAR</Link>
+        <div className="flex justify-center text-blue3 px-4">
+            <div className="w-full max-w-md my-12">
+                <div className="bg-blue3 text-white text-xl font-semibold py-3 text-center rounded-t-lg border border-black">
+                    Login
+                </div>
+                <div className="bg-blue1 border-x border-b border-black rounded-b-lg">
+                    <div className="flex justify-center py-6 px-6">
+                        <form className="flex flex-col w-full space-y-4">
+                            <GenericFormField id="user" type="text" placeholder="E-mail, CPF ou CNPJ" autoComplete="username">Usuário</GenericFormField>
+                            <GenericFormField id="senha" type="password" placeholder="Digite sua senha" autoComplete="current-password">Senha</GenericFormField>
+                            
+                            <div className="flex flex-col sm:flex-row sm:justify-between text-sm py-2 gap-2">
+                                <Link to="/auth/password/forgot" className="underline">Esqueci a senha</Link>
+                                <label htmlFor="rememberMe" className="flex items-center">
+                                    <input type="checkbox" id="rememberMe" className="mr-2"/>
+                                    <span>Lembre de mim</span>
+                                </label>
                             </div>
-                            <GenericBlueButton color={3} link="/candidates/1/dashboard">Entrar</GenericBlueButton> {/* A implementar */}
-                        </div>
-                    </form>
-
+                            
+                            <div className="flex flex-col sm:flex-row sm:justify-between sm:items-end pt-12 gap-4">
+                                <div className="flex flex-col text-sm">
+                                    <span>É sua primeira vez?</span>
+                                    <Link to="/auth/register/main" className="underline pt-1 font-semibold">CADASTRAR</Link>
+                                </div>
+                                <GenericBlueButton color={3} link="/candidates/1/dashboard">Entrar</GenericBlueButton>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
