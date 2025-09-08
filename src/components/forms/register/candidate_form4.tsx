13e058337d4c29e053cb40be94d837fc97de51bc
LucianoMazar <lmazaraojr@gmail.com>
2025-09-08 08:00:52 -0300

    Adição da parte de informações sobre acessibilidade para o cadastro de candidato

--- src/components/forms/register/candidate_form4.tsx
+++ src/components/forms/register/candidate_form4.tsx
@@ -1,10 +1,19 @@
-// [TODO] - Procurar encaixar o formulario com os dados do banco gerado pelo denaiel
-// [TODO] - Procurar mais aprofundamente como será feita a coleta desses dados
+// [TODO] - Linkar as APIs para o o conteúdo do Select, pegando dados especificos de subtipo de necessidade dependendo do tipo
+
+import GenericFormField from "../generic_form_field";
+
 
 export default function CandidateForm4 (){
     return (
         <form className="flex-col text-start space-y-8">
             <h2 className="font-semibold text-[1.3rem]">Informações sobre acessibilidade</h2>
+            <GenericFormField id="candidate_type_register" type="select" options={['W', 'I', 'P']} placeholder="Select">Tipo de Necessidade</GenericFormField>
+            <GenericFormField id="candidate_sub_type_register" type="select" options={['W', 'I', 'P']} placeholder="Select">Subtipo de Necessidade</GenericFormField>
+            <GenericFormField id="candidate_sub_type_register" type="file">Laudo Médico</GenericFormField>
+            <div className="bg-blue4 p-6 space-y-4 rounded-lg">
+                <h3 className="font-semibold text-[20px]">Por que pedimos um laudo médico?</h3>
+                <p>Falar um pouco da importânica, tratamento dos dados etc etc Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod est optio cupiditate deserunt error necessitatibus mollitia asperiores quo porro, vero doloribus quam voluptatem repudiandae! Dolor quis perferendis animi voluptatem corrupti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eaque aliquam earum atque sequi expedita, laboriosam minus sint error vel unde nisi. Fugit facere dolorum rerum soluta laborum optio placeat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore maiores, rerum molestiae fugit itaque quod rem corporis consectetur suscipit blanditiis, dolor ab sit eaque aliquam illo obcaecati? Maxime, odio dolor.</p>
+            </div>
         </form>
     )
 }
