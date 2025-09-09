e95d21129f275fa0179f3e5838d1b0ed3d830338
LucianoMazar <lmazaraojr@gmail.com>
2025-09-08 21:07:20 -0300

    minima alteração do footer

--- src/components/structure/footer.tsx
+++ src/components/structure/footer.tsx
@@ -28,7 +28,7 @@ export default function Footer(){
 
                 {/* Para Candidatos */}
                 <div>
-                    <h3 className="font-bold mb-4 text-blue1">Candidatos</h3>
+                    <h3 className="font-bold mb-4 text-blue1">Usuarios</h3>
                     <ul className="space-y-2 text-sm">
                         <li><Link to="/auth/register/main" className="hover:text-blue4 transition-colors">Cadastre-se</Link></li>
                         <li><Link to="/auth/login" className="hover:text-blue4 transition-colors">Fazer Login</Link></li>
