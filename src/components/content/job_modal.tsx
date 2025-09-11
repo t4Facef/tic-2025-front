7238630e1cc92911c8665d41f80ba829854e1d29
LucianoMazar <lmazaraojr@gmail.com>
2025-09-11 09:32:24 -0300

    Alinhamento de dados entre visualização e criação de vagas e correção do mockdata

--- src/components/content/job_modal.tsx
+++ src/components/content/job_modal.tsx
@@ -1,3 +1,5 @@
+// [TODO] - Fazer com que os campos só apareçam na visualização se existir o dado no banco (pra evitar ficar espaço vazio)
+
 import { useEffect } from "react";
 import TagContainer from "./tag_container";
 import ProfileLink from "../profile/profile_link";
@@ -9,9 +11,9 @@ interface JobModalProps {
   jobData: JobData;
 }
 
-export default function JobModal({ 
-  open, 
-  onClose, 
+export default function JobModal({
+  open,
+  onClose,
   jobData
 }: JobModalProps) {
 
@@ -21,7 +23,7 @@ export default function JobModal({
     } else {
       document.body.style.overflow = 'unset';
     }
-    
+
     return () => {
       document.body.style.overflow = 'unset';
     };
@@ -30,34 +32,57 @@ export default function JobModal({
   if (!open) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-4 space-y-8" onClick={onClose}>
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-4" onClick={onClose}>
       <div className="w-full max-w-4xl max-h-[95vh] flex flex-col" onClick={(e) => e.stopPropagation()}>
-        <div className="bg-blue1 p-6 rounded-t-xl shadow-lg flex-1 overflow-auto">
-          <div className="flex flex-1 justify-between">
-            <div className="flex-[7]">
-              <h2 className="text-xl font-bold mb-4 text-[70px] py-7">{jobData.title}</h2>
-              <div className="">{jobData.description.split('\n')[0]}</div>
+        <div className="bg-blue1 p-6 rounded-t-xl shadow-lg flex-1 overflow-auto space-y-6">
+          <div className="flex justify-between">
+            <div className="flex-[7] space-y-4">
+              <div className="space-y-1">
+                <h2 className="font-bold text-4xl">{jobData.title}</h2>
+                <p className="text-xs">Período de Inscrição: {jobData.startDate.toLocaleDateString('pt-BR')} até {jobData.endDate.toLocaleDateString('pt-BR')}</p>
+              </div>
+              <div>{jobData.description.split('\n')[0]}</div>
             </div>
             <div className="bg-white rounded-full flex justify-center items-center w-40 h-40 m-3 ml-9 border border-black overflow-hidden">
               <ProfileLink id={jobData.idEmpresa} imgPath={jobData.companyLogo}></ProfileLink>
             </div>
           </div>
-          <div className="my-3">
-            <div className="whitespace-pre-line text-gray-700 leading-relaxed">
-              {jobData.description}
+
+          <div className="whitespace-pre-line text-gray-700 leading-relaxed">
+            {jobData.description}
+          </div>
+
+          <div className="space-y-2">
+            <p className="font-medium">Características da vaga</p>
+            <div className="bg-blue4 rounded-lg p-2">
+              <div className="flex justify-between">
+                <span>Modalidade: <strong>{jobData.typeWork}</strong></span>
+                <span>Localização: <strong>{jobData.location}</strong></span>
+              </div>
+              <div className="flex justify-between">
+                <span>Nivel da Vaga: <strong>{jobData.workLevel}</strong></span>
+                <span>Horário de Trabalho: <strong>{jobData.timeShift}</strong></span>
+              </div>
+              <div className="flex justify-between">
+                <span>Pagamento: <strong>{jobData.payment}</strong></span>
+                <span>Contrato: <strong>{jobData.typeContract}</strong></span>
+              </div>
             </div>
           </div>
-          <div className="space-y-8">
+
+          <div className="space-y-4">
             <TagContainer tags={jobData.skillsTags}>Habilidades esperadas</TagContainer>
             <TagContainer tags={jobData.supportTags}>Apoio da empresa</TagContainer>
           </div>
         </div>
-        <div className="bg-white border border-y-black flex justify-center py-2">
-          <span>Você é {jobData.compatibility}% compátivel com essa vaga</span>
+
+        <div className="bg-white border border-y-black flex justify-center">
+          <span className="m-2">Você é {jobData.compatibility}% compatível com essa vaga</span>
         </div>
-        <div className="flex flex-1">
-          <button className="bg-blue3 text-white flex-[5] border-black border-[2px] text-[35px] rounded-bl-3xl py-2" onClick={onClose}>Fechar</button>
-          <button className="bg-blue3 text-white flex-[5] border-black border-[2px] text-[35px] rounded-br-3xl py-2" onClick={onClose}>Inscrever</button>
+
+        <div className="flex">
+          <button className="bg-blue3 text-white flex-1 border-black border-2 text-4xl rounded-bl-3xl" onClick={onClose}>Fechar</button>
+          <button className="bg-blue3 text-white flex-1 border-black border-2 text-4xl rounded-br-3xl" onClick={onClose}>Inscrever</button>
         </div>
       </div>
     </div>
--- src/pages/job_new.tsx
+++ src/pages/job_new.tsx
@@ -20,11 +20,17 @@ export default function JobForm() {
     company: '',
     companyLogo: '',
     location: '',
-    type: '',
     description: '',
     skillsTags: [],
     supportTags: [],
-    compatibility: 0
+    compatibility: 0,
+    startDate: new Date(),
+    endDate: new Date(),
+    typeContract: '',
+    typeWork: '',
+    payment: '',
+    workLevel: '',
+    timeShift: ''
   })
 
   const handleSalarioChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
@@ -39,65 +45,79 @@ export default function JobForm() {
     <div className="my-16 mx-64 space-y-8">
       <div>
         <div className="p-3 text-white text-[1.5rem] bg-blue3 border border-black min-w-[28rem] rounded-t-lg">
-        <h2>
-          {isEditing ? 'Edite sua vaga' : 'Cadastre sua vaga'}
-        </h2>
-      </div>
-      <form className="flex-col text-start space-5 text-blue3 bg-blue1 rounded-b-lg px-12">
-        <div className="p-3 space-y-5">
-          <GenericFormField id="job_registration" type="text" placeholder="Titulo da vaga" onChange={(e) => setDataForm(prev => ({...prev, title: e.target.value}))}>
-            Nome da vaga
-          </GenericFormField>
-          <div className="flex gap-24">
-            <GenericFormField id="start_date" type="date">
-              Início das inscrições
+          <h2>
+            {isEditing ? 'Edite sua vaga' : 'Cadastre sua vaga'}
+          </h2>
+        </div>
+        <form className="flex-col text-start space-5 text-blue3 bg-blue1 rounded-b-lg px-12">
+          <div className="p-3 space-y-5">
+            <GenericFormField id="job_title" type="text" placeholder="Titulo da vaga" onChange={(e) => setDataForm(prev => ({ ...prev, title: e.target.value }))}>
+              Nome da vaga
             </GenericFormField>
-            <GenericFormField id="end_date" type="date">
-              Fim das inscrições
+            <div className="flex gap-24">
+              <GenericFormField id="start_date" type="date" onChange={(e) => setDataForm(prev => ({ ...prev, startDate: new Date(e.target.value) }))}>
+                Início das inscrições
+              </GenericFormField>
+              <GenericFormField id="end_date" type="date" onChange={(e) => setDataForm(prev => ({ ...prev, endDate: new Date(e.target.value) }))}>
+                Fim das inscrições
+              </GenericFormField>
+            </div>
+            <MarkdownEditor
+              value={description}
+              onChange={setDescription}
+              placeholder="Descreva sua vaga aqui..."
+              label="Descrição da vaga"
+            />
+            <GenericFormField
+              id="job_salary"
+              type="currency"
+              placeholder="R$ 0,00"
+              value={salario}
+              onChange={handleSalarioChange}
+            >
+              Salário
             </GenericFormField>
+            <GenericFormField
+              id="job_level"
+              type="select"
+              options={["Estágio", "Júnior", "Pleno", "Sênior"]}
+            >
+              Nível da vaga
+            </GenericFormField>
+            <GenericFormField id="job_workload" type="text" placeholder="Horas">
+              Carga Horária
+            </GenericFormField>
+            <div>
+              <p>Caracteristicas da vaga</p>
+              <div className="p-6 bg-blue4 rounded-lg space-y-8">
+                <GenericFormField
+                  id="job_type_work"
+                  type="radio"
+                  options={["Remoto", "Presencial", "Híbrido"]}
+                  onChange={(e) => setDataForm(prev => ({ ...prev, typeWork: e.target.value }))}
+                >
+                  <strong>Tipo de Trabalho</strong>
+                </GenericFormField>
+                <GenericFormField
+                  id="job_type_contract"
+                  type="radio"
+                  options={["CLT", "PJ", "Estágio", "Freelancer", "Temporário"]}
+                  onChange={(e) => setDataForm(prev => ({ ...prev, typeContract: e.target.value }))}
+                >
+                  <strong>Tipo de Contrato</strong>
+                </GenericFormField>
+              </div>
+            </div>
+            <TagContainer tags={[]} edit={true}>
+              Habilidades
+            </TagContainer>
           </div>
-          <MarkdownEditor
-            value={description}
-            onChange={setDescription}
-            placeholder="Descreva sua vaga aqui..."
-            label="Descrição da vaga"
-          />
-          <GenericFormField
-            id="job_salary"
-            type="currency"
-            placeholder="R$ 0,00"
-            value={salario}
-            onChange={handleSalarioChange}
-          >
-            Salário
-          </GenericFormField>
-          <GenericFormField
-            id="job_level"
-            type="select"
-            options={["Estágio", "Júnior", "Pleno", "Sênior"]}
-          >
-            Nível da vaga
-          </GenericFormField>
-          <GenericFormField id="job_workload" type="text" placeholder="Horas">
-            Carga Horária
-          </GenericFormField>
-          <GenericFormField
-            id="job_characteristics"
-            type="radio"
-            options={["Remoto", "Presencial", "Híbrido"]}
-          >
-            Características da vaga
-          </GenericFormField>
-          <TagContainer tags={[]} edit={true}>
-            Habilidades
-          </TagContainer>
-        </div>
-        <div className="flex justify-end p-4">
-          <GenericBlueButton color={3} onClick={handleSubmit}>{isEditing ? 'Salvar Alterações' : 'Cadastrar Vaga'}</GenericBlueButton>
-        </div>
-      </form>
+          <div className="flex justify-end p-4">
+            <GenericBlueButton color={3} onClick={handleSubmit}>{isEditing ? 'Salvar Alterações' : 'Cadastrar Vaga'}</GenericBlueButton>
+          </div>
+        </form>
       </div>
-      
+
       <div>
         <h3 className="font-medium">Pré-visualização da Vaga</h3>
         <JobPosition jobData={dataForm}></JobPosition>
