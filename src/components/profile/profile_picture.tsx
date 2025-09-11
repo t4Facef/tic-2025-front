2e645a97096733f3bfff0275ac54e39edd9f18ed
LucianoMazar <lmazaraojr@gmail.com>
2025-09-11 20:02:25 -0300

    Alteração de estilo botão de notificação

--- src/components/profile/profile_picture.tsx
+++ src/components/profile/profile_picture.tsx
@@ -2,7 +2,7 @@ import NotificationBox from "../content/notification_box";
 
 export default function ProfilePicture() {
   return (
-    <div className="flex items-center pr-6 space-x-2">
+    <div className="flex items-center pr-6 gap-6">
       <NotificationBox/>
       <img 
         src="/img/profile-default.png" //Colocar como props depois para receber um caminho para a imagem de perfil
