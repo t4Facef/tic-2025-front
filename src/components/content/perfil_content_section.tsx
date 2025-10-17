f90dc081d8b633b989250b0ae87961c2c7d2ce04
LucianoMazar <lmazaraojr@gmail.com>
2025-10-17 12:39:33 -0300

    feat: Adição da estrutura para edição de paginas

--- src/components/content/perfil_content_section.tsx
+++ src/components/content/perfil_content_section.tsx
@@ -1,12 +1,8 @@
-// [TODO] - Adicionar condicionais para se existe ou não um ou resultado para a caixa (se não existir descrição, não mostrar o titulo "descrição" e por ai vai)
-// [TODO] - Adicionar visualização dependendo do usario (se for dono da conta consegue editar, se não só visualizar)
-// [TODO] - Adicionar um componente proprio para alteração na própria pagina, ao invés de usar um botão genérico de link
-// [TODO] - Procurar um icone de setinha e implementar abrir e fechar
-
 import { useState } from "react";
 import GenericBlueButton from "../buttons/generic_blue_button"
 import GenericFormField from "../forms/generic_form_field";
 import { EDUCATION_TYPES, CONTRACT_TYPES, EDUCATION_STATUS } from "../../data/constants/select_options";
+import { PenLine } from "lucide-react";
 
 interface InfoType {
     formationType?: string;
@@ -30,6 +26,7 @@ interface PerfilContentSectionProps {
 export default function PerfilContentSection({ title, info, description, type, edit = false, onAdd }: PerfilContentSectionProps) {
     const [isAdding, setIsAdding] = useState(false)
     const [newInfoType, setNewInfoType] = useState<InfoType>({} as InfoType)
+    const [editingIndex, setEditingIndex] = useState<number | null>(null)
     const isFormation = type === "formacao"
     return (
         <div className="bg-blue1 px-6 py-2 rounded-xl">
@@ -43,39 +40,57 @@ export default function PerfilContentSection({ title, info, description, type, e
                 )}
                 <hr className="border-black mb-6" />
                 <div className="space-y-4 mt-4">
-                    {info.map((item, index) => (
-                        <div key={index} className="p-4 bg-blue4 rounded-lg">
-                            <div className="py-3">
-                                <p className="text-[13px]">{item.formationType}</p>
-                                <p className="text-[22px] font-medium">{item.course} - {item.institut}</p>
-                                <p className="text-[15px]">{item.startDate} até {item.endDate} - {item.status}</p>
+                    {info.map((item, index) => {
+                        const isEditing = editingIndex === index
+                        if(!isEditing) return (
+                            <div key={index} className="p-4 bg-blue4 rounded-lg">
+                                <div className="flex justify-between">
+                                    <div className="py-3">
+                                        <p className="text-[13px]">{item.formationType}</p>
+                                        <p className="text-[22px] font-medium">{item.course} - {item.institut}</p>
+                                        <p className="text-[15px]">{item.startDate} até {item.endDate} - {item.status}</p>
+                                    </div>
+                                    {edit && <div>
+                                        <button className="p-2 bg-white rounded-full shadow-md hover:bg-slate-50" onClick={() => setEditingIndex(isEditing ? null : index)}>
+                                            <PenLine />
+                                        </button>
+                                    </div>}
+
+                                </div>
+                                <h2 className="font-bold text-lg mt-4 mb-2">Descrição</h2>
+                                <p className="mb-4">
+                                    {item.desc}
+                                </p>
                             </div>
-                            <h2 className="font-bold text-lg mt-4 mb-2">Descrição</h2>
-                            <p className="mb-4">
-                                {item.desc}
-                            </p>
-                        </div>
-                    ))}
+                        )
+                        else{
+                            // Adicionar possobilidade de edição
+                        }
+                    })}
                     {isAdding && <div className="p-4 bg-blue4 rounded-lg">
                         <div className="py-3 space-y-1">
                             <p className="font-semibold">Adicionando {isFormation ? 'Nova Formação' : 'Nova Experiência'}</p>
-                            <GenericFormField id="tipo" type="select" placeholder="Selecione" options={isFormation ? EDUCATION_TYPES : CONTRACT_TYPES} onChange={(e) => setNewInfoType((prev) => ({...prev, formationType: e.target.value}))}>{isFormation ? "Tipo de Formação" : "Tipo de Contrato"}</GenericFormField>
+                            <GenericFormField id="tipo" type="select" placeholder="Selecione" options={isFormation ? EDUCATION_TYPES : CONTRACT_TYPES} onChange={(e) => setNewInfoType((prev) => ({ ...prev, formationType: e.target.value }))}>{isFormation ? "Tipo de Formação" : "Tipo de Contrato"}</GenericFormField>
                             <div className="flex gap-8">
-                                <GenericFormField id="curso" onChange={(e) => setNewInfoType((prev) => ({...prev, course: e.target.value}))}>{isFormation ? "Curso" : "Título/Cargo"}</GenericFormField>
-                                <GenericFormField id="local" onChange={(e) => setNewInfoType((prev) => ({...prev, institut: e.target.value}))}>{isFormation ? "Instituição" : "Empresa"}</GenericFormField>
+                                <GenericFormField id="curso" onChange={(e) => setNewInfoType((prev) => ({ ...prev, course: e.target.value }))}>{isFormation ? "Curso" : "Título/Cargo"}</GenericFormField>
+                                <GenericFormField id="local" onChange={(e) => setNewInfoType((prev) => ({ ...prev, institut: e.target.value }))}>{isFormation ? "Instituição" : "Empresa"}</GenericFormField>
                             </div>
                             <div className="flex gap-8">
-                                <GenericFormField id="startDate" type="date" onChange={(e) => setNewInfoType((prev) => ({...prev, startDate: e.target.value}))}>Data de Início</GenericFormField>
-                                <GenericFormField id="endDate" type="date" onChange={(e) => setNewInfoType((prev) => ({...prev, endDate: e.target.value}))}>Data de Finalização</GenericFormField>
-                                {isFormation && <GenericFormField id="status" type="select" options={EDUCATION_STATUS} onChange={(e) => setNewInfoType((prev) => ({...prev, status: e.target.value}))}>Status</GenericFormField>}
+                                <GenericFormField id="startDate" type="date" onChange={(e) => setNewInfoType((prev) => ({ ...prev, startDate: e.target.value }))}>Data de Início</GenericFormField>
+                                <GenericFormField id="endDate" type="date" onChange={(e) => setNewInfoType((prev) => ({ ...prev, endDate: e.target.value }))}>Data de Finalização</GenericFormField>
+                                {isFormation && <GenericFormField id="status" type="select" options={EDUCATION_STATUS} onChange={(e) => setNewInfoType((prev) => ({ ...prev, status: e.target.value }))}>Status</GenericFormField>}
                             </div>
                         </div>
-                        <GenericFormField id="descricao" type="textarea" onChange={(e) => setNewInfoType((prev) => ({...prev, desc: e.target.value}))}>Descrição</GenericFormField>
+                        <GenericFormField id="descricao" type="textarea" onChange={(e) => setNewInfoType((prev) => ({ ...prev, desc: e.target.value }))}>Descrição</GenericFormField>
                     </div>
                     }
                 </div>
                 {edit && (
                     <div className="flex justify-end gap-5 m-3">
+                        {isAdding && <GenericBlueButton color={3} size="md" onClick={() => {
+                            setIsAdding(false)
+                            setNewInfoType({} as InfoType)
+                        }}>Cancelar</GenericBlueButton>}
                         <GenericBlueButton color={3} size="md" onClick={async () => {
                             if (isAdding) {
                                 await onAdd?.(newInfoType)
